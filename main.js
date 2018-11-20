a2e7241e12857e69d3a65667a05f9bb82aeb7572
Kasheena Walton <kasheenaw@gmail.com>
2018-11-19 16:27:15 -0800

    added content

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ fetch('https://api.exchangeratesapi.io/latest')
             .then(response => response.json())
             .then(data => { 
                 console.log("Current Exchange Rates", data) 
-            });
+                for (let datum in data) {
+                    console.log('test', datum.rates)
+                    for (let item in datum.rates) 
+                        console.log('item', item);                    
+                }});
+    
+                for (let datum in data){
+                    function data(datumrates){
+                        return rates;
+                    }
+                    }
 
-(currency => {
+                
+// (currency => {
 
-const graph = document.createElement('div');
-graph.setAttribute('class','graph');
+// const graph = document.createElement('div');
+// graph.setAttribute('class','graph');
 
-const graph = document.createElement('graph');
-graph.textContent = currency.value;
+// const graph = document.createElement('graph');
+// graph.textContent = currency.value;
 
-        });
+//         });
